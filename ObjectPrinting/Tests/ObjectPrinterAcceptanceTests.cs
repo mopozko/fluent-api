eca47ae8bbafa422c1686646355b5203655bca0b
Andrey <mopozko123@gmail.com>
2017-11-24 15:18:38 +0500

    добавил модульные тесты для ObjectPrinter

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -16,16 +16,8 @@ namespace ObjectPrinting.Tests
             person = new Person
             {
                 Name = "John",
-                Surname = "Smith",
                 Age = 19,
                 Height = 1.95,
-                Parent = new Person()
-                {
-                    Age = 45,
-                    Name = "Mary",
-                    Surname = "Smith",
-                    Height = 1.65
-                }
             };
         }
 
@@ -33,24 +25,23 @@ namespace ObjectPrinting.Tests
 		public void Demo()
 		{
             var printer = ObjectPrinter.For<Person>()
-                //1. Исключить из сериализации свойства определенного типа   ++
-                .Excluding<Guid>()
-                //2. Указать альтернативный способ сериализации для определенного типа ++
-                .Printing<int>()
-                    .Using(p => "hide")
-                //3. Для числовых типов указать культуру
-                .Printing<double>()
-                    .Using(CultureInfo.InvariantCulture)
-                //4. Настроить сериализацию конкретного свойства   ++
-                .Printing(p => p.Name)
-                    .Using(p => p + " +")
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                .Printing(p => p.Surname)
-                  .TrimmedToLength(4)
-                //6. Исключить из сериализации конкретного свойства +++
-                .Excluding(p => p.Surname)
-                ;
-
+               //1.Исключить из сериализации свойства определенного типа++
+               .Excluding<Guid>()
+               //2. Указать альтернативный способ сериализации для определенного типа ++
+               .Printing<int>()
+                   .Using(p => "hide")
+               //3. Для числовых типов указать культуру
+               .Printing<double>()
+                   .Using(CultureInfo.InvariantCulture)
+               //4. Настроить сериализацию конкретного свойства   ++
+               .Printing(p => p.Name)
+                   .Using(p => p + " +as")
+               //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+               .Printing(p => p.Name)
+                 .TrimmedToLength(10)
+               //6. Исключить из сериализации конкретного свойства +++
+               .Excluding(p => p.Name)
+               ;
             string s1 = printer.PrintToString(person);
 
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию ++
@@ -63,8 +54,167 @@ namespace ObjectPrinting.Tests
             Console.WriteLine(s3);
 		}
 
+        [Test]
+        public void PrintToString_ObjectExtensionTesting1()
+        {
+            var result = ObjectPrinter.For<Person>()
+                .PrintToString(person);
+            var extensionResult = person.PrintToString();
+            extensionResult.Should().Be(result);
+        }
 
+        [Test]
+        public void PrintToString_ObjectExtensionTesting2()
+        {
+            var result = ObjectPrinter.For<Person>()
+                .Excluding<Guid>()
+                .Printing<string>()
+                    .Using(x => x.ToUpper())
+                .Printing<double>()
+                    .Using(CultureInfo.InvariantCulture)
+                .Printing(x => x.Name)
+                    .Using(x => x + "asdasd")
+                .Printing(x => x.Name)
+                    .TrimmedToLength(3)
+                .PrintToString(person);
+            var extensionResult = person.PrintToString(conf => conf
+                .Excluding<Guid>()
+                .Printing<string>()
+                    .Using(x => x.ToUpper())
+                .Printing<double>()
+                    .Using(CultureInfo.InvariantCulture)
+                .Printing(x => x.Name)
+                    .Using(x => x + "asdasd")
+                .Printing(x => x.Name)
+                    .TrimmedToLength(3));
+            extensionResult.Should().Be(result);
+        }
+        [Test]
+        public void PrintToString_WithDefaultPrinterConfiguration()
+        {
+            var result = person.PrintToString();
+            var expected =
+                "Person\r\n" +
+                "\tId = 00000000-0000-0000-0000-000000000000\r\n" +
+                "\tName = John\r\n" +
+                "\tHeight = 1,95\r\n"+
+                "\tAge = 19\r\n";
+            result.Should().Be(expected);
+        }
+        [Test]
+        public void PrintToString_ExcludeStringAndGuid()
+        {
+            var result = person.PrintToString(conf => conf
+            .Excluding<string>()
+            .Excluding<Guid>());
+            var expected =
+                "Person\r\n" +
+                "\tHeight = 1,95\r\n" +
+                "\tAge = 19\r\n";
+            result.Should().Be(expected);
+        }
+        [Test]
+        public void PrintToString_ExcludeAgeProperty()
+        {
+            var result = person.PrintToString(conf => conf.Excluding(x => x.Age));
+            var expected =
+                "Person\r\n" +
+                "\tId = 00000000-0000-0000-0000-000000000000\r\n" +
+                "\tName = John\r\n" +
+                "\tHeight = 1,95\r\n";
+            result.Should().Be(expected);
+        }
+        [Test]
+        public void PrintToString_SetCustomPrintingConfigurationForType()
+        {
+            var result = person.PrintToString(conf => conf.Printing<Guid>().Using(x => "007"));
+            var expected =
+                "Person\r\n" +
+                "\tId = 007\r\n" +
+                "\tName = John\r\n" +
+                "\tHeight = 1,95\r\n" +
+                "\tAge = 19\r\n";
+            result.Should().Be(expected);
+        }
+        [Test]
+        public void PrintToString_SetCustomPrintingConfigurationForProperty()
+        {
+            var result = person.PrintToString(conf => conf.Printing( x=> x.Name).Using(x => x + " Smith"));
+            var expected =
+                "Person\r\n" +
+                "\tId = 00000000-0000-0000-0000-000000000000\r\n" +
+                "\tName = John Smith\r\n" +
+                "\tHeight = 1,95\r\n" +
+                "\tAge = 19\r\n";
+            result.Should().Be(expected);
+        }
+        [Test]
+        public void PrintToString_NullObject()
+        {
+            person = null;
+            var result = person.PrintToString();
+            var expected ="null";
+            result.Should().Be(expected);
+        }
 
+        [Test]
+        public void PrintToString_ObjectWithNullProperty()
+        {
+            person.Name = null;
+            var result = person.PrintToString();
+            var expected =
+                "Person\r\n" +
+                "\tId = 00000000-0000-0000-0000-000000000000\r\n" +
+                "\tName = null\r\n" +
+                "\tHeight = 1,95\r\n" +
+                "\tAge = 19\r\n";
+            result.Should().Be(expected);
+        }
 
+        [Test]
+        public void PrintToString_SetDigitsCulture()
+        {
+            var result = person.PrintToString(conf => 
+                    conf.Printing<double>()
+                    .Using(CultureInfo.InvariantCulture));
+            var expected =
+                "Person\r\n" +
+                "\tId = 00000000-0000-0000-0000-000000000000\r\n" +
+                "\tName = John\r\n" +
+                "\tHeight = 1.95\r\n" + // double теперь с точкой
+                "\tAge = 19\r\n";
+            result.Should().Be(expected);
+        }
+        [Test]
+        public void PrintToString_TrimmedStringProperty()
+        {
+            var result = person.PrintToString(conf =>
+                conf.Printing(p => p.Name)
+                .TrimmedToLength(2));
+            var expected =
+                "Person\r\n" +
+                "\tId = 00000000-0000-0000-0000-000000000000\r\n" +
+                "\tName = Jo\r\n" +// длина теперь 2
+                "\tHeight = 1,95\r\n" + 
+                "\tAge = 19\r\n";
+            result.Should().Be(expected);
+        }
+        [Test]
+        public void PrintToString_TrimmedStringPropertyWithCustomPrinting()
+        {
+            var result = person.PrintToString(conf =>
+                conf
+                .Printing(p => p.Name)
+                    .Using(p => p.ToUpper())
+                .Printing(p => p.Name)
+                    .TrimmedToLength(3));
+            var expected =
+                "Person\r\n" +
+                "\tId = 00000000-0000-0000-0000-000000000000\r\n" +
+                "\tName = JOH\r\n" +// Обрезание у кастомной печати
+                "\tHeight = 1,95\r\n" +
+                "\tAge = 19\r\n";
+            result.Should().Be(expected);
+        }
     }
 }
